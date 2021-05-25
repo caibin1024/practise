bbbc8bc5108099f8a8b91c40e8c1cb569e18e3cb
caibin <caibin1024@163.com>
2021-05-25 18:27:25 +0800

    circle组件完善中

---
+++ src/components/ideaOfCircle/MyCircle.vue
@@ -0,0 +1,28 @@
+<template>
+<h1>{{diameter}}</h1>
+  <div id="circle" @click="isShow=!isShow">
+    <my-circle v-if="isShow"></my-circle>
+    <my-circle v-if="isShow"></my-circle>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['diameter'],
+  name: "my-circle",
+  data() {
+    return {
+      isShow: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+#circle {
+  width: 500px;
+  height: 500px;
+  background-color: rgba(0, 0, 0, 0);
+  border-radius: 50%;
+}
+</style>
--- src/view/IdeaOfCircle.vue
+++ src/view/IdeaOfCircle.vue
@@ -1,29 +1,28 @@
 <template>
-    <div>
-        <el-row v-for="count in times" :key="count">
-            <el-col v-for="count1 in times" :key="count+'/'+count1" span="1" >
-                <div id="cricle"></div>
-            </el-col>
-        </el-row>
-    </div>
+  <div>
+    <my-circle :diameter="1000"></my-circle>
+  </div>
 </template>
 
 
 <script>
+import myCircle from "../components/ideaOfCircle/MyCircle";
 export default {
-    data(){
-        return{
-            times:5
-        }
-    },
-}
+  data() {
+    return {
+      times: 5,
+    };
+  },
+  components: { myCircle
+   },
+};
 </script>
 
 <style scoped>
-.cricle{
-    width: 10px;
-    height: 10px;
-    background-color:green;
-    border-radius: 5px;
+.cricle {
+  width: 10px;
+  height: 10px;
+  background-color: green;
+  border-radius: 5px;
 }
 </style>
